ea2cc052ebb2740d9a1bc4e7cab3e4b21f9016a9
Henry Borchers <henryborchers@yahoo.com>
2017-02-11 20:57:35 -0600

    Window can now manage children.

--- lib/window.c
+++ lib/window.c
@@ -10,6 +10,8 @@ Window *window_create() {
     Window *temp = NULL;
     temp = (Window *) malloc(sizeof(Window));
     if (temp) {
+        temp->children.container_size = CHILDREN_CONTAINER_START_SIZE;
+        temp->children.children = calloc(sizeof(void**), CHILDREN_CONTAINER_START_SIZE);
         window_init(temp);
     }
     return temp;
@@ -23,18 +25,26 @@ int window_init(Window *temp) {
     temp->background.g = 0;
     temp->background.b = 0;
     temp->background.a = 0;
-    temp->height = -1;
-    temp->width = -1;
+    temp->size.height = -1;
+    temp->size.width = -1;
+    temp->children.amount = 0;
+    temp->children.container_size = CHILDREN_CONTAINER_START_SIZE;
     return 0;
 }
 
 void window_destroy(Window **pWindow) {
-    Window *tmp = (*pWindow);
-    tmp->height = -1;
-    tmp->width = -1;
+    Window *tmp = *(pWindow);
+    tmp->size.height = -1;
+    tmp->size.width = -1;
     SDL_DestroyRenderer(tmp->renderer);
     SDL_DestroyTexture(tmp->texture);
     SDL_DestroyWindow(tmp->window);
+    for(int i = 0; i < tmp->children.amount; i++){
+        Pane *p = (Pane*)tmp->children.children[i];
+        if(p != NULL){
+            pane_destroy(&p);
+        }
+    }
     free(tmp);
     *pWindow = NULL;
 }
@@ -42,14 +52,14 @@ void window_destroy(Window **pWindow) {
 int window_open(Window *w) {
     int ret = 0;
 
-    if ((ret = SDL_CreateWindowAndRenderer(w->width, w->height, SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN, &w->window,
+    if ((ret = SDL_CreateWindowAndRenderer(w->size.width, w->size.height, SDL_WINDOW_RESIZABLE | SDL_WINDOW_SHOWN, &w->window,
                                            &w->renderer)) != 0) {
         fprintf(stderr, "Unable to create renderer and/or window\n");
         return ret;
     }
     SDL_SetWindowTitle(w->window, w->title);
-    w->texture = SDL_CreateTexture(w->renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, w->width,
-                                   w->height);
+    w->texture = SDL_CreateTexture(w->renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, w->size.width,
+                                   w->size.height);
     if (NULL == w->texture) {
         fprintf(stderr, "Unable to create texture\n");
         ret = -1;
@@ -61,12 +71,18 @@ int window_refresh(Window *w) {
     SDL_Event e;
     while (SDL_PollEvent(&e)) {
         if (e.type == SDL_QUIT) {
-//            puts("Quit");
             return 1;
         }
     }
-
+    SDL_GetWindowSize(w->window, &w->size.width, &w->size.height);
     window_draw_background(w);
+    for(int i = 0; i < w->children.amount; i++){
+        Pane* child = w->children.children[i];
+        if(NULL != child){
+            pane_refresh(child, w);
+        }
+    }
+    SDL_RenderPresent(w->renderer);
     return 0;
 }
 
@@ -74,5 +90,5 @@ void window_draw_background(const Window *w) {
     SDL_SetRenderTarget(w->renderer, NULL);
     SDL_SetRenderDrawColor(w->renderer, w->background.r, w->background.g, w->background.b, w->background.a);
     SDL_RenderClear(w->renderer);
-    SDL_RenderPresent(w->renderer);
+
 }
